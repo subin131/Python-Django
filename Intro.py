19bdc6a9b0fd674916d219512586823df43e718f
satyal <satyalsubin33@gmail.com>
2022-07-05 21:30:40 +0545

    some notes about django

---
+++ Intro.py
@@ -0,0 +1,42 @@
+#conversion of react into javascript
+
+#server side: backend and database
+#client side: frontend
+#backend framework: Django
+
+
+
+#framework is a skeleton for a website or blue print for a website 
+#reusable code is a piece of code that can be used in multiple projects
+#in framework where it provide pre defined skeleton for a website
+
+#project is a complete web app
+
+#modulalr structure divide into part-part
+
+
+
+
+#steps to create a project
+
+#to create a project:
+#django-admin startproject mysite
+
+#to create app
+#django-admin startapp auth_app
+
+
+
+#python manage.py .... comand
+# to run server
+#django manage.py runserver
+
+# to regsiter
+#by adding the app_name in settings.py file in installed list
+
+
+
+#setting have all the details of the project
+#setting.py is a file that contains all the details of the project
+#url.py is a file give a routing details
+#
